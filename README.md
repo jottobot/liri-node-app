b287616a1915b6e86e9b26f01a6fc6fa1f1ed019
Jordynn Otto <j_taylor92@yahoo.com>
2019-05-01 17:36:54 -0700

    working on readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # liri-node-app
+
+
+## Overview
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data. In this case, LIRI will tell the user concerts, spotify songs, and movies.
+
+## Requirements 
+* Node installation
+* These NPM packages:
+  * Require
+  * Node-spotify-api
+  * FS
+  * Moment
+  * Dotenv
+  * Axios
+  * Bandsintown
+  * OMDB
+* Your own personal spotify API key and secret
+
+## Installation
+
+## Usage
+Liri can be used with the commands "concert-this", "spotify-this", or "movie-this". By typing in the command then specifically what you are looking for, Liri will print your desired information. For example, if you were looking for information about the movie Jaws, you would type "Node liri.js movie-this jaws" into the terminal.
+
+If not information is provided, the defualt song is "The Sign" and default movie is "Mr. Nobody."
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var axios = require('axios');
 var fs = require('fs');
 var Spotify = require('node-spotify-api');
+var moment = require("moment");
 require('dotenv').config();
 
 var keys = require('./keys.js');
@@ -41,15 +42,16 @@ function concertThis(artist) {
       '/events?app_id=codingbootcamp'
     )
     .then(function (response) {
-      console.log(response.data);
+      // console.log(response.data);
+
       if (!response.data[0]) {
         console.log('No concerts found!');
       } else {
-        for (var i = 0; i < response.data.length; i++) {
-          console.log('-------------');
+        for (var i = 0; i < 5; i++) {
+          console.log('------------------------');
           console.log('The concert venue is: ' + response.data[i].venue.name);
-          // console.log("The location is: " + venue location);
-          // console.log("The date is: " + date of event);
+          console.log("The location is: " + response.data[i].venue.city);
+          console.log("The date is: " + moment(response.data[i].datetime).format("MMM Do YY"));
         }
       }
     });
@@ -60,33 +62,33 @@ function spotifyThis(song) {
     song = "The sign";
   }
   // else {
-    var spotify = new Spotify({
-      id: keys.spotify.id,
-      secret: keys.spotify.secret
-    });
+  var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
 
-    spotify.search(
-      {
-        type: 'track',
-        query: song
-      },
-      function (err, data) {
-        if (err) {
-          console.log('Error occurred: ' + err);
-          return;
-        }
-
-        var songs = data.tracks.items;
-
-        for (var i = 0; i < 5; i++) {
-          console.log('Song name: ' + songs[i].name);
-          console.log('Preview: ' + songs[i].preview_url);
-          console.log('Album: ' + songs[i].album.name);
-          console.log('-----------------------------------');
-        }
+  spotify.search(
+    {
+      type: 'track',
+      query: song
+    },
+    function (err, data) {
+      if (err) {
+        console.log('Error occurred: ' + err);
+        return;
       }
-    );
-  }
+
+      var songs = data.tracks.items;
+
+      for (var i = 0; i < 5; i++) {
+        console.log('------------------------');
+        console.log('Song name: ' + songs[i].name);
+        console.log('Preview: ' + songs[i].preview_url);
+        console.log('Album: ' + songs[i].album.name);
+      }
+    }
+  );
+}
 // };
 
 function movieThis(movie) {
@@ -101,6 +103,7 @@ function movieThis(movie) {
       '&y=&plot=full&tomatoes=true&apikey=trilogy'
     )
     .then(function (response) {
+      console.log('------------------------');
       console.log('Title: ' + response.data.Title);
       console.log('Release year: ' + response.data.Year);
       console.log('IMDB: ' + response.data.imdbRating);
@@ -110,4 +113,26 @@ function movieThis(movie) {
       console.log('Plot: ' + response.data.Plot);
       console.log('Cast: ' + response.data.Actors);
     });
+}
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+
+    // Then split it by commas (to make it more readable) in one array
+    var dataArr = data.split(",");
+
+    command1 = dataArr[0];
+    if (command1 === "concert-this") {
+      concertThis(dataArr[1]);
+    } else if (command1 === "spotify-this") {
+      spotifyThis(dataArr[1]);
+    } else if (command1 === "movie-this") {
+      movieThis(dataArr[1]);
+    }
+  })
 }
